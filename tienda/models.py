7f6f71e2f8d5df17a2d76864db77a5e33be36bc9
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-05-26 18:48:51 +0200

    se modifica el fixture con el  nuevo modelo

--- tienda/models.py
+++ tienda/models.py
@@ -119,8 +119,7 @@ class LineaPedido(models.Model):
 class ProductosTerceros(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.FloatField(default=1.0) 
-
-    
+  
     def __str__(self):
         return self.nombre
     
