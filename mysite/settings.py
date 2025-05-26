063129a0d8ddd204e8898ffc09b997c24ffaae28
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-05-26 19:36:23 +0200

    implementacion de OAuth2 servidor OK

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,7 +47,27 @@ INSTALLED_APPS = [
     'django_bootstrap5',
     'django_bootstrap_icons',
     'rest_framework',
+    'oauth2_provider',
 ]
+
+
+#este es para la autenticacion de token
+OAUTH2_PROVIDER = {
+    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Acceso a los grupos'},
+    "ACCESS_TOKEN_EXPIRE_SECONDS": 5000000000,
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    ),
+
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('tienda.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/v1/', include('tienda.api_urls')),
+    path('oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')) #para los tokens
     
 ]
 
